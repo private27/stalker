326251e130311750a854b5b42cef654702140e0a
KairatMengdikarayev <54745003+KairatMengdikarayev@users.noreply.github.com>
2020-03-10 15:05:17 +0300

    Add files via upload

---
+++ stalker.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Скрипт для удаление сохранений на кириллице в игре S.T.A.L.K.E.R.
+# Все информации об обновлении/ошибки, и инструкция в файле readme.txt.
+
+import os
+
+LETTERS = ('а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й',
+		 		'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 
+		 		'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я')
+
+# Путь к сохранениям.
+saves = r'D:\Games\games_ready\S.T.A.L.K.E.R. - Объединенный Пак 2\appdata\savedgames'
+
+# Открываем файлы.
+for top, dirs, files in os.walk(saves):
+	print(f'Общее количество файлов - {len(files)}.')
+	for name in files:
+		
+		# Проверяем файлы на кириллицу.
+		for letter in LETTERS:
+			if letter in name:
+
+				# Если файл на кириллице, тогда удаляем.
+				print(f'{name} - удалена.')
+				os.remove(os.path.join(saves, name))
+				break
+
+# Некорректно работает.
+# print(f'Количество файлов после удаление - {len(files)}.')
